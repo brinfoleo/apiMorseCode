724fa02438001c41b5b50fcd40e69f78a154a944
brinfoleo <brinfo.leo@gmail.com>
2024-03-25 10:44:22 +0000

    Swagger implementation

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
 using  apiMorseEncoder.MorseEncoder;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+ 
+//Services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "MORSE CODE!");
 
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Simple API to encode and decode Morse code
 
+//Swagger
+>dotnet add package Microsoft.AspNetCore.OpenApi --version 7.0.10
+>dotnet add package Swashbuckle.AspNetCore
